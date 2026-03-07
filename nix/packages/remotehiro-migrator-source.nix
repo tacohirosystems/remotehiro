49ae8c19602c83a087dd65bf0c52f982805c1d56
sekun <sekun@duck.com>
2026-03-07 03:18:31 +0100

    chore: Bump data migrations

--- nix/packages/remotehiro-migrator-source.nix
+++ nix/packages/remotehiro-migrator-source.nix
@@ -6,9 +6,9 @@
 
     (pkgs.fetchgit {
       url = "https://forgejo.quoll-owl.ts.net/remotehiro/data-migrations";
-      rev = "a1bda64fad632bd3658d9dca1a820e410eb6c1cb";
+      rev = "ad6775ec5dd29d3ba0a8ba1ed2b49ccc68269382";
       # sha256 = pkgs.lib.fakeSha256;
-      sha256 = "sha256-Vm0eF7+UclYKW/hrYTuzR4czmLP79b1YyDGLFrMie+8=";
+      sha256 = "sha256-5FQelNZboyrp0nY5Mvk6JGlobIKkp4BMgk/V8AahILY=";
       name = "data-migrations";
      })
   ];
